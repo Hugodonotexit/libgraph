ecd3e7786fefab576f20102d9710b38101f151a8
Hugodonotexit <hugohk955.au@gmail.com>
2024-06-09 16:15:43 +0930

    not working

--- example/test-2.cpp
+++ example/test-2.cpp
@@ -2,9 +2,17 @@
 
 int main() {
     sgt::SDLG graph;
-    sgt::Func func("10*sin(x)+100");
-    graph.pushFunc(func);
-    graph.setCurve(0);
+    //sgt::Func func("10*sin(x)+100");
+    //graph.pushFunc(func);
+    sgt::Vectorlf a(-100,-100);
+    sgt::Vectorlf b(100,100);
+    graph.setLine(a,b,sgt::Colour::Green());
+    sgt::Vectorlf c(100,100);
+    sgt::Vectorlf d(-100,-100);
+    graph.setLine(c,d,sgt::Colour::Blue());
+    sgt::Vectorlf e(100,5);
+    sgt::Vectorlf f(-100,5);
+    graph.setLine(e,f,sgt::Colour::Yellow());
     graph.startLoop();
     return 0;
 }
